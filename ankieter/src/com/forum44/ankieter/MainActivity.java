1cadb194519ea40a8ddcabb0fba6ae8821f1ab4a
Piotrek Nasinski <ppp@piotrek>
2012-07-22 19:30:43 +0200

    proby ze stanem internetu

--- ankieter/src/com/forum44/ankieter/MainActivity.java
+++ ankieter/src/com/forum44/ankieter/MainActivity.java
@@ -1,14 +1,19 @@
 package com.forum44.ankieter;
+
+import android.net.ConnectivityManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.telephony.TelephonyManager;
 
@@ -20,170 +25,140 @@ import com.forum44.ankieter.parametry;
 
 public class MainActivity extends Activity {
 
-	public int  a=0;
-	public int  sekund=1000*1; //co jaki przedzia³ czasu
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        
-       // ListView mainListView = (ListView) findViewById( R.id.mainListView );  
-        
-        
-        String[] planets = new String[] { "Mercury", "Venus", "Earth", "Mars",
-                "Jupiter", "Saturn", "Uranus", "Neptune","Mercury", "Venus", "Earth", "Mars",
-                "Jupiter", "Saturn", "Uranus", "Neptune","Mercury", "Venus", "Earth", "Mars",
-                "Jupiter", "Saturn", "Uranus", "Neptune"};  
-        
-        
-ArrayList<String> planetList = new ArrayList<String>();
-planetList.addAll( Arrays.asList(planets) );
+	public int a = 0;
+	public int sekund = 1000 * 1; // co jaki przedzia³ czasu
 
-// Create ArrayAdapter using the planet list.
-ArrayAdapter listAdapter = new ArrayAdapter<String>(this,R.layout.simplerow, planetList);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-// Add more planets. If you passed a String[] instead of a List<String> 
-// into the ArrayAdapter constructor, you must not add more items. 
-// Otherwise an exception will occur.
-//listAdapter.add( "Ceres" );
-//listAdapter.add( "Pluto" );
-//listAdapter.add( "Haumea" );
-//listAdapter.add( "Makemake" );
-//listAdapter.add( "Eris" );
+		// ListView mainListView = (ListView) findViewById( R.id.mainListView );
 
-// a to lista 
-// Set the ArrayAdapter as the ListView's adapter.
-//mainListView.setAdapter( listAdapter );      
- 
-    }
+		String[] planets = new String[] { "Mercury", "Venus", "Earth", "Mars",
+				"Jupiter", "Saturn", "Uranus", "Neptune", "Mercury", "Venus",
+				"Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune",
+				"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn",
+				"Uranus", "Neptune" };
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+		ArrayList<String> planetList = new ArrayList<String>();
+		planetList.addAll(Arrays.asList(planets));
 
-    public void sprawdz(View v) 
-    {
-    	
-    	Button b = (Button)findViewById(R.id.button2);
-    	b.setEnabled(false);
-    	
-    	Timer zegarek;
+		// Create ArrayAdapter using the planet list.
+		ArrayAdapter listAdapter = new ArrayAdapter<String>(this,
+				R.layout.simplerow, planetList);
 
-    	zegarek = new Timer();
-    	
-    	zegarek.schedule(new TimerTask() {
-    		@Override
-    		public void run() {
-    		timerMethod();
-    		}
-    		
-    		}, 0, sekund);
-    	
-    }
-    
-    public void wroc(View v)
-    {
-    	 setContentView(R.layout.activity_main);
-    }
-    
-    
-    public void nastepny(View v)
-    {
-    	 setContentView(R.layout.drugieokno);
-    }
-    
-    
-    private void timerMethod()
-    {
-      this.runOnUiThread(doSomething);
-    }
-    
+		// Add more planets. If you passed a String[] instead of a List<String>
+		// into the ArrayAdapter constructor, you must not add more items.
+		// Otherwise an exception will occur.
+		// listAdapter.add( "Ceres" );
+		// listAdapter.add( "Pluto" );
+		// listAdapter.add( "Haumea" );
+		// listAdapter.add( "Makemake" );
+		// listAdapter.add( "Eris" );
 
-    
-    private Runnable doSomething = new Runnable() {
-    	public void run() {
-    	 	TextView t = (TextView)findViewById(R.id.cyfry);
-        	t.setText(""+a); 
-        	a++;
-    	   }
-    	};
-    
-    	public void pokaz(View v) 
-        {
-        	 TextView t = (TextView)findViewById(R.id.producent);
-             parametry test = new parametry();
-        	 t.setText(test.napis);        	  
-        }	
-    	
-    	
-    public void kliknij(View v) 
-    {
-    	 TextView t = (TextView)findViewById(R.id.daneimei);
-    	 TelephonyManager TelephonyMgr = (TelephonyManager)getSystemService(TELEPHONY_SERVICE);
-    	 String szImei = TelephonyMgr.getDeviceId();
-    	 t.setText(szImei);
-    	 
-    	 TextView t1 = (TextView)findViewById(R.id.model);
-    	 t1.setText(Build.MANUFACTURER);
-    	 TextView t2 = (TextView)findViewById(R.id.producent);
-    	 t2.setText(Build.MODEL);
-    	 
-    	  
-    }
-    
-    
-    public static final String TABLE_COMMENTS = "comments";
-	public static final String COLUMN_ID = "_id";
-	public static final String COLUMN_COMMENT = "comment";
+		// a to lista
+		// Set the ArrayAdapter as the ListView's adapter.
+		// mainListView.setAdapter( listAdapter );
 
-	private static final String DATABASE_NAME = "commments.db";
-	private static final int DATABASE_VERSION = 1;
-	
-	
-	// Database creation sql statement
-		private static final String DATABASE_CREATE = "create table "
-				+ TABLE_COMMENTS + "(" + COLUMN_ID
-				+ " integer primary key autoincrement, " + COLUMN_COMMENT
-				+ " text not null);";
-	 
-	public void onCreate(SQLiteDatabase database) {
-		database.execSQL(DATABASE_CREATE);
 	}
 
-	 
-	
-	
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		Log.w(MainActivity.class.getName(),
-				"Upgrading database from version " + oldVersion + " to "
-						+ newVersion + ", which will destroy all old data");
-		db.execSQL("DROP TABLE IF EXISTS " + TABLE_COMMENTS);
-		onCreate(db);
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
 	}
+
+	public void sprawdz(View v) {
+
+		Button b = (Button) findViewById(R.id.button2);
+		b.setEnabled(false);
+
+		Timer zegarek;
+
+		zegarek = new Timer();
+
+		zegarek.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				timerMethod();
+			}
+
+		}, 0, sekund);
+
+	}
+
+	public void wroc(View v) {
+		setContentView(R.layout.activity_main);
+	}
+
+	public void nastepny(View v) {
+		setContentView(R.layout.drugieokno);
+	}
+
+	private void timerMethod() {
+		this.runOnUiThread(doSomething);
+	}
+
+	private Runnable doSomething = new Runnable() {
+		public void run() {
+			TextView t = (TextView) findViewById(R.id.cyfry);
+			t.setText("" + a);
+			a++;
+		}
+	};
+
+	public void pokaz(View v) {
+		TextView t = (TextView) findViewById(R.id.producent);
+		parametry test = new parametry();
+		t.setText(test.napis);
+	}
+
+	public void kliknij(View v) {
+		TextView t = (TextView) findViewById(R.id.daneimei);
+		TelephonyManager TelephonyMgr = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+		String szImei = TelephonyMgr.getDeviceId();
+		t.setText(szImei);
+
+		TextView t1 = (TextView) findViewById(R.id.model);
+		t1.setText(Build.MANUFACTURER);
+		TextView t2 = (TextView) findViewById(R.id.producent);
+		t2.setText(Build.MODEL);
+
+	}
+
+	public void staninternetu(View V) {
+		int a;
+		lokalizacja sesja = new lokalizacja();
+		
+		
+		a = sesja.polaczenia(getApplicationContext());
+		TextView t = (TextView) findViewById(R.id.internet);
+		t.setText("" + a);
+
+	}
+
 	
 	
-     
-    public void podaj_dane_telefonu(View v) 
-    {
-    	
-       	 
-    	 
-    	 String m_szDevIDShort =    	Build.BOARD+ Build.BRAND +
-    	        	Build.CPU_ABI + Build.DEVICE +
-    	        	Build.DISPLAY + Build.HOST +
-    	        	Build.ID+ Build.MANUFACTURER+
-    	        	Build.MODEL + Build.PRODUCT +
-    	        	Build.TAGS + Build.TYPE+
-    	        	Build.USER ; //13 digits
-    	 			
-//    	 TextView t = (TextView)findViewById(R.id.daneimei);
-//    	 t.setText("IMEI: "+m_szDevIDShort);
-    
-    	 			
-    }
-    
+	//zwraca 2 dla polaczenia WIFII
+	//zwraca 1 dla polaczenia GSM
+	//zwraca 0 dla braku internetu
+	
+
+	/*
+	 * public Arrays podaj_dane_telefonu(View v) {
+	 * 
+	 * 
+	 * Arrays wynik=; String m_szDevIDShort = Build.BOARD+ Build.BRAND +
+	 * Build.CPU_ABI + Build.DEVICE + Build.DISPLAY + Build.HOST + Build.ID+
+	 * Build.MANUFACTURER+ Build.MODEL + Build.PRODUCT + Build.TAGS +
+	 * Build.TYPE+ Build.USER ; //13 digits return wynik;
+	 * 
+	 * // TextView t = (TextView)findViewById(R.id.daneimei); //
+	 * t.setText("IMEI: "+m_szDevIDShort);
+	 * 
+	 * 
+	 * }
+	 */
+
 }
--- ankieter/src/com/forum44/ankieter/lokalizacja.java
+++ ankieter/src/com/forum44/ankieter/lokalizacja.java
@@ -1,5 +1,78 @@
 package com.forum44.ankieter;
 
-public class lokalizacja {
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+
+public class lokalizacja extends MainActivity  {
+
+	 LocationManager myManager;
+	   
+	    private Location savedLocation = null;
+	    
+	    private LocationManager locationManager;
+	    Location location; 
+	
+	
+	    public int polaczenia(Context context) {
+			ConnectivityManager connManager = null;
+			String networkService = Context.CONNECTIVITY_SERVICE;
+			int wynik=0;
+
+			/*String provider = Settings.Secure.getString(getContentResolver(),
+					Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+
+			if (!provider.contains("gps")) { // if gps is disabled
+				final Intent poke = new Intent();
+				poke.setClassName("com.android.settings",
+						"com.android.settings.widget.SettingsAppWidgetProvider");
+				poke.addCategory(Intent.CATEGORY_ALTERNATIVE);
+				poke.setData(Uri.parse("3"));
+				sendBroadcast(poke);
+			}*/
+
+			connManager = (ConnectivityManager) getSystemService(networkService);
+
+			if (connManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).isConnected())
+			{
+				wynik = 1;
+				
+			}
+			if (connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).isConnected())
+			{
+				wynik = 2;
+				
+			}
+				
+			
+			if (connManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE)
+					.isConnected()
+					|| connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
+							.isConnected()) {
+
+				
+
+			} else {
+				wynik = 0;
+			}
+			
+			
+			
+			return wynik;
+
+		}
+	
 }
